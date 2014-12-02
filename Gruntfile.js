311495404a2c1d733e9f6ad47778b1016507776e
xiaowu <admin@xuexb.com>
2014-12-02 12:18:14 +0800

    添加server配置

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,10 @@
 module.exports = function(grunt) {
     'use strict';
 
+    var Url = require('url');
+    var fs = require('fs');
+    var marked = require('marked');
+
     var config = grunt.file.readJSON('package.json'); //读取 package.json 配置
 
 
@@ -18,17 +22,62 @@ module.exports = function(grunt) {
         jshintrc: true
     }
     jshint.all = {
-        src: ['./Gruntfile.js', './src/**/*.js']
+        src: ['./src/**/*.js']
     }
 
     // http服务配置
     connect.server = {
         options: {
-            port: '81',
+            port: '8800',
             base: './',
             hostname: '127.0.0.1',
             keepalive: true,
-            open: true
+            open: true,
+            middleware: function(connect, options, middlewares) {
+                // inject a custom middleware into the array of default middlewares
+                middlewares.unshift(function(req, res, next) {
+                    var url = Url.parse(req.url).pathname,
+                        html, tpl;
+
+                    // 如果不是md文档则下一个
+                    if (url.lastIndexOf('.md') !== url.length - 3) {
+                        return next();
+                    }
+
+                    url = '.'+ url;
+
+                    if (!fs.existsSync(url)) {
+                        return res.end('404');
+                    }
+
+                    html = fs.readFileSync(url).toString();
+
+                    // console.log(html);
+
+                    if(!html){
+                        return res.end('error');
+                    }
+
+                    // 读取模板内容
+                    tpl = fs.readFileSync('./markdown.tpl').toString();
+
+                    // md化
+                    html = marked(html);
+
+                    // 替换md文档到模板里
+                    tpl = tpl.replace('{{body}}', html);
+
+                    // 生成标题
+                    html = html.match(/<h1(?:.*?)>(.+?)<\/h1>/) || ['', url.substr(url.lastIndexOf('/') + 1) || 'md文档'];
+
+                    // 替换标题
+                    tpl = tpl.replace('{{title}}', html[1]);
+
+                    res.end(tpl);
+                });
+
+                return middlewares;
+            }
         }
     }
 
@@ -42,8 +91,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-connect');
 
-    grunt.registerTask('test', function(){
-        console.log('test');
+    grunt.registerTask('server', function() {
         grunt.task.run('connect')
     });
 }
